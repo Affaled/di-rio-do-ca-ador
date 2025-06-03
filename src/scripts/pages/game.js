b3b58c0bc265a91dc6396231cb16e5f9081d0b82
Affaled <affaledl@gmail.com>
2025-06-02 21:01:56 -0300

    começo da fase de exibição das informações salvas sobre o personagem e a localização

--- src/scripts/pages/game.js
+++ src/scripts/pages/game.js
@@ -4,16 +4,56 @@ import {
 	removeFromLocal,
 } from "/src/scripts/local-saves.js";
 import setLocation from "/src/scripts/set-location.js";
+import Barbarian from "/src/scripts/templates/character/Barbarian.js";
+import Hermit from "/src/scripts/templates/character/Hermit.js";
 
 $(document).ready(function () {
-	const character = loadFromLocal("character") || null;
-	if (!character) {
+	const data = loadFromLocal("character") || null;
+	let character;
+
+	if (!data) {
 		alert("Nenhum personagem encontrado. Crie um personagem primeiro.");
 		window.location.hash = "/novo-personagem";
 		return;
 	}
 
-	const locationName = setLocation();
+	if (data.profession === "Bárbaro") {
+		character = new Barbarian(data.name);
+	} else if (data.profession === "Eremita") {
+		let petType = data.pet?.type;
 
-	$(".game__location").html(`<h2>Local: ${locationName}</h2>`);
+		if (petType === "Voador") petType = "FlyingPet";
+		else if (petType === "Lutador") petType = "FightingPet";
+		else if (petType === "Rastreador") petType = "TrackerPet";
+		character = new Hermit(data.name, petType);
+	} else {
+		alert("Profissão desconhecida. Crie um personagem válido.");
+		window.location.hash = "/novo-personagem";
+		return;
+	}
+
+	character.attributes = data.attributes;
+	character.advantages = data.advantages;
+	character.maxLifePoints = data.maxLifePoints;
+	character.image = data.image;
+	character.backpack.items = data.backpack;
+
+	if (character.pet && data.pet) {
+		character.pet.maxLifePoints = data.pet.maxLifePoints;
+		character.pet.damage = data.pet.damage;
+		character.pet.abilities = data.pet.abilities;
+	}
+
+	const locationDetails = setLocation();
+
+	$(".game__location").html(`
+		<p>Cidade: ${locationDetails.name}</p>
+		<p>${locationDetails.size.name}: ${locationDetails.size.description}</p>
+		<p>${locationDetails.type.name}: ${locationDetails.type.description}</p>
+		`);
+
+	$(".game__character").html(`
+		<p>Nome: ${character.name}</p>
+		<p>Classe: ${character.profession}</p>
+		`);
 });
--- src/scripts/set-location.js
+++ src/scripts/set-location.js
@@ -5,19 +5,88 @@ export default function setLocation() {
 	const prefixB = ["Lind", "Rosen", "Degen", "Ans", "Kings", "Bay"];
 	const sufixA = ["burg", "gard", "heim", "hut", "wood", "ville"];
 	const sufixB = ["born", "stad", "sen", "ia", "berg", "bach"];
+	const citySize = [
+		{
+			name: "Povoado",
+			description: "Apenas algumas casas simples ou mesmo improvisadas.",
+		},
+		{
+			name: "Aldeia",
+			description:
+				"Algumas casas, um poço e uma pequena taverna ou 'casa comum'.",
+		},
+		{
+			name: "Vilarejo",
+			description:
+				"Não possui muros. Possui um pequeno templo e uma hospedaria ou taverna.",
+		},
+		{
+			name: "Vila",
+			description:
+				"Possui uma simples defesa de madeira, um templo, uma mercearia e uma hospedaria ou taverna.",
+		},
+		{
+			name: "Pequena Cidade",
+			description:
+				"Possui muros, um templo, um mercado  e alguns estabelecimentos.",
+		},
+		{
+			name: "Grande Cidade",
+			description:
+				"Possui muros altos, um grande templo, um grande mercado e muitos estabelecimentos.",
+		},
+	];
+	const cityType = [
+		{
+			name: "Raça",
+			description:
+				"Os habitantes são de uma raça diferente e única desta região.",
+		},
+		{
+			name: "Ruínas",
+			description:
+				"Esta localidade foi destruída há algum tempo, mas segue tentando sobreviver desde então.",
+		},
+		{
+			name: "Reiligião",
+			description:
+				"Os habitantes em geral são muito religiosos e os sacerdotes são as autoridades ali.",
+		},
+		{
+			name: "Comércio",
+			description:
+				"O comércio é o principal meio de sustento desta localidade.",
+		},
+		{
+			name: "Rural",
+			description:
+				"É uma região basicamente rural e os habitantes são bem pacíficos.",
+		},
+		{
+			name: "Militar",
+			description:
+				"Esta localidade foi criada com uma função militar, embora já possa ter perdido esse objetivo.",
+		},
+	];
 	const pick = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-	const table = [
-		() => pick(prefixA) + pick(sufixA),
-		() => pick(prefixB) + pick(sufixA),
-		() => pick(prefixA) + pick(sufixB) + pick(sufixA),
-		() => "Nova " + pick(sufixB) + pick(sufixA),
-		() => pick(prefixA) + pick(sufixB),
+	const nameTable = [
 		() => pick(prefixB) + pick(sufixB),
+		() => pick(prefixA) + pick(sufixB),
+		() => "Nova " + pick(sufixB) + pick(sufixA),
+		() => pick(prefixA) + pick(sufixB) + pick(sufixA),
+		() => pick(prefixB) + pick(sufixA),
+		() => pick(prefixA) + pick(sufixA),
 	];
 
 	const idx = rolld6() - 1;
-	const locationName = table[idx]();
+	const locationName = nameTable[idx]();
+	const locationSize = citySize[idx];
+	const locationType = cityType[idx];
 
-	return locationName;
+	return {
+		name: locationName,
+		size: locationSize,
+		type: locationType,
+	};
 }
